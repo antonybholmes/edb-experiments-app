7556d6090c773fef633a2eab06381f8381239ed5
Antony Holmes <antony.b.holmes@gmail.com>
2018-01-13 17:35:12 -0500

    Updates and bug fixes.

--- edu.columbia.rdf.edb.experiments.app/src/main/java/edu/columbia/rdf/edb/experiments/app/search/folder/SearchFolderDialog.java
+++ edu.columbia.rdf.edb.experiments.app/src/main/java/edu/columbia/rdf/edb/experiments/app/search/folder/SearchFolderDialog.java
@@ -128,7 +128,7 @@ public class SearchFolderDialog extends ModernDialogTaskWindow {
    * Creates the ui.
    */
   private final void createUi() {
-    // this.getContentPane().add(new JLabel("Change " +
+    // this.getWindowContentPanel().add(new JLabel("Change " +
     // getProductDetails().getProductName() + " settings", JLabel.LEFT),
     // BorderLayout.PAGE_START);
 
